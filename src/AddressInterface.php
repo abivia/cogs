de11a436808f70d6bfb6844c92763c374eec512d
Alan Langford <instance1@gmail.com>
2022-08-11 15:45:06 -0400

    Extract AddressProperties interface from AddressInterface to support consistent representation of non-address objects that contain (sparse) address related data. Allow null values to be returned.

--- src/AddressInterface.php
+++ src/AddressInterface.php
@@ -6,60 +6,16 @@ use DateTimeInterface;
 use UnitEnum;
 
 /**
- * Interface for international postal addresses.
+ * Interface for an international postal address object.
+ *
+ * This represents an address stored in a data set and connected to
+ * a Party or some other entity.
  *
  * Adapted from https://github.com/commerceguys/addressing
  *
  */
-interface AddressInterface
+interface AddressInterface extends AddressProperties
 {
-    /**
-     * Gets the first line of address block.
-     *
-     * @return string The first line of the address block.
-     */
-    public function addressLine1(): string;
-
-    /**
-     * Gets the second line of address block.
-     *
-     * @return string The second line of the address block.
-     */
-    public function addressLine2(): string;
-
-    /**
-     * Gets the administrative area.
-     *
-     * Called the "state" in the United States, "region" in France, "province" in Italy,
-     * "county" in Great Britain, "prefecture" in Japan, etc.
-     *
-     * @return string The administrative area. A subdivision code if there
-     *                are predefined subdivision at this level.
-     */
-    public function administrativeArea(): string;
-
-    /**
-     * Gets the two-letter country code.
-     *
-     * This is a CLDR country code, since CLDR includes additional countries
-     * for addressing purposes, such as Canary Islands (IC).
-     *
-     * @return string The two-letter country code.
-     */
-    public function countryCode(): string;
-
-    /**
-     * Gets the dependent locality (i.e neighbourhood).
-     *
-     * When representing a double-dependent locality in Great Britain, includes
-     * both the double-dependent locality and the dependent locality,
-     * e.g. "Whaley, Langwith".
-     *
-     * @return string The administrative area. A subdivision code if there
-     *                are predefined subdivisions at this level.
-     */
-    public function dependentLocality(): string;
-
     /**
      * The date that the address becomes in effect.
      *
@@ -75,41 +31,11 @@ interface AddressInterface
     public function id(): mixed;
 
     /**
-     * Gets the locale.
-     *
-     * Allows the initially-selected address format / subdivision translations
-     * to be selected and used the next time this address is modified.
-     *
-     * @return string The locale.
-     */
-    public function locale(): string;
-
-    /**
-     * Gets the locality (i.e. city).
-     *
-     * Some countries do not use this field; their address lines are sufficient
-     * to locate an address within a sub-administrative area.
-     *
-     * @return string The administrative area. A subdivision code if there
-     *                are predefined subdivision at this level.
-     */
-    public function locality(): string;
-
-    /**
      * The name of the relationship (primary, billing, shipping, technical, admin, etc.)
      *
      * @return string
      */
     public function name(): string;
-
-    /**
-     * Gets the postal code.
-     *
-     * The value is often alphanumeric.
-     *
-     * @return string The postal code.
-     */
-    public function postalCode(): string;
 
     /**
      * Retrieve the address specified by the id parameter.
@@ -118,15 +44,6 @@ interface AddressInterface
      * @return AddressInterface|null Null if no AddressInterface with this ID is found.
      */
     static public function retrieve(mixed $id): ?AddressInterface;
-
-    /**
-     * Gets the sorting code.
-     *
-     * For example, CEDEX in France.
-     *
-     * @return string The sorting code.
-     */
-    public function sortingCode(): string;
 
     /**
      * The state of the relationship (eg. active, former)
---
+++ src/AddressProperties.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Abivia\Cogs;
+
+/**
+ * Interface for international postal addresses.
+ *
+ * This defines the fields contained in an address.
+ *
+ * Adapted from https://github.com/commerceguys/addressing
+ *
+ */
+interface AddressProperties
+{
+    /**
+     * Gets the first line of address block.
+     *
+     * @return string|null The first line of the address block.
+     */
+    public function addressLine1(): ?string;
+
+    /**
+     * Gets the second line of address block.
+     *
+     * @return string|null The second line of the address block.
+     */
+    public function addressLine2(): ?string;
+
+    /**
+     * Gets the administrative area.
+     *
+     * Called the "state" in the United States, "region" in France, "province" in Italy,
+     * "county" in Great Britain, "prefecture" in Japan, etc.
+     *
+     * @return string|null The administrative area. A subdivision code if there
+     *                are predefined subdivision at this level.
+     */
+    public function administrativeArea(): ?string;
+
+    /**
+     * Gets the two-letter country code.
+     *
+     * This is a CLDR country code, since CLDR includes additional countries
+     * for addressing purposes, such as Canary Islands (IC).
+     *
+     * @return string|null The two-letter country code.
+     */
+    public function countryCode(): ?string;
+
+    /**
+     * Gets the dependent locality (i.e neighbourhood).
+     *
+     * When representing a double-dependent locality in Great Britain, includes
+     * both the double-dependent locality and the dependent locality,
+     * e.g. "Whaley, Langwith".
+     *
+     * @return string|null The administrative area. A subdivision code if there
+     *                are predefined subdivisions at this level.
+     */
+    public function dependentLocality(): ?string;
+
+    /**
+     * Gets the locale.
+     *
+     * Allows the initially-selected address format / subdivision translations
+     * to be selected and used the next time this address is modified.
+     *
+     * @return string|null The locale.
+     */
+    public function locale(): ?string;
+
+    /**
+     * Gets the locality (i.e. city).
+     *
+     * Some countries do not use this field; their address lines are sufficient
+     * to locate an address within a sub-administrative area.
+     *
+     * @return string|null The administrative area. A subdivision code if there
+     *                are predefined subdivision at this level.
+     */
+    public function locality(): ?string;
+
+    /**
+     * Gets the postal code.
+     *
+     * The value is often alphanumeric.
+     *
+     * @return string|null The postal code.
+     */
+    public function postalCode(): ?string;
+
+    /**
+     * Gets the sorting code.
+     *
+     * For example, CEDEX in France.
+     *
+     * @return string|null The sorting code.
+     */
+    public function sortingCode(): ?string;
+
+}
